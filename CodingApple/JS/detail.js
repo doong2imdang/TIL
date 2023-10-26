f53b252210a056641d0bb2691c0da0e62e8bbb03
KIm Seo Young <jmh7367@naver.com>
2023-10-27 00:01:21 +0900

    10. 탭 기능 만들며 배우는  for 반복문 2_숙제

--- CodingApple/JS/detail.js
+++ CodingApple/JS/detail.js
@@ -3,35 +3,51 @@
 // - 버튼O에 orange 클래스명 추가
 // - 모든 div에 붙은 show 클래스명 제거
 // - divO에 show 클래스명 추가
-document.addEventListener("DOMContentLoaded", function () {
-  const tabButtons = document.querySelectorAll(".tab-button");
-  const tabContents = document.querySelectorAll(".tab-content");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const tabButtons = document.querySelectorAll(".tab-button");
+//   const tabContents = document.querySelectorAll(".tab-content");
 
-  tabButtons.forEach((button, index) => {
-    button.addEventListener("click", function () {
-      // 모든 탭 컨텐츠를 숨김
-      tabContents.forEach((content) => {
-        content.classList.remove("show");
-      });
+//   tabButtons.forEach((button, index) => {
+//     button.addEventListener("click", function () {
+//       // 모든 탭 컨텐츠를 숨김
+//       tabContents.forEach((content) => {
+//         content.classList.remove("show");
+//       });
 
-      // 현재 클릭한 버튼에 해당하는 컨텐츠만 보임
-      tabContents[index].classList.add("show");
+//       // 현재 클릭한 버튼에 해당하는 컨텐츠만 보임
+//       tabContents[index].classList.add("show");
 
-      // 모든 탭 버튼 스타일을 초기화
-      tabButtons.forEach((btn) => {
-        btn.classList.remove("orange");
-      });
+//       // 모든 탭 버튼 스타일을 초기화
+//       tabButtons.forEach((btn) => {
+//         btn.classList.remove("orange");
+//       });
 
-      // 현재 클릭한 버튼에 스타일을 적용
-      button.classList.add("orange");
-    });
-  });
+//       // 현재 클릭한 버튼에 스타일을 적용
+//       button.classList.add("orange");
+//     });
+//   });
 
-  // 초기 상태 설정: 첫번쨰 탭을 활성화
-  tabButtons[0].click();
-});
+//   // 초기 상태 설정: 첫번쨰 탭을 활성화
+//   tabButtons[0].click();
+// });
 
 // 숙제. for 반복문으로 풀어보기
+let Buttons = document.querySelectorAll(".tab-button");
+let Contents = document.querySelectorAll(".tab-content");
+for (let i = 0; i < Buttons.length; i++) {
+  Buttons[i].addEventListener("click", function () {
+    // 모든 버튼에서 'orange' 클래스 제거
+    Buttons.forEach((button) => button.classList.remove("orange"));
+    // 현재 클릭한 버튼에 'orange' 클래스 추가
+    Buttons[i].classList.add("orange");
+
+    // 모든 콘텐츠에서 'show' 클래스 제거
+    Contents.forEach((content) => content.classList.remove("show"));
+    // 현재 클릭한 버튼에 해당하는 콘텐츠에 'show' 클래스 추가
+    Contents[i].classList.add("show");
+  });
+}
+
 // 응용. 탭이 4개, 5개로 늘어나도 잘 작동하는 코드?
 
 // jQuery //

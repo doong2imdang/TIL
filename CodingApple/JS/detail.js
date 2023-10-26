7db03b415da9fb53921f2ae05be697de6c7b5ad6
KIm Seo Young <jmh7367@naver.com>
2023-10-26 17:46:18 +0900

    9. 탭기능 만들며 배우는 for 반복문_숙제

--- CodingApple/JS/detail.js
+++ CodingApple/JS/detail.js
@@ -3,8 +3,32 @@
 // - 버튼O에 orange 클래스명 추가
 // - 모든 div에 붙은 show 클래스명 제거
 // - divO에 show 클래스명 추가
-document.querySelector(".tab-button").addEventListener("click", function () {
-  // 숙제. 버튼 3개 기능 전부 숙제로 해오기
+document.addEventListener("DOMContentLoaded", function () {
+  const tabButtons = document.querySelectorAll(".tab-button");
+  const tabContents = document.querySelectorAll(".tab-content");
+
+  tabButtons.forEach((button, index) => {
+    button.addEventListener("click", function () {
+      // 모든 탭 컨텐츠를 숨김
+      tabContents.forEach((content) => {
+        content.classList.remove("show");
+      });
+
+      // 현재 클릭한 버튼에 해당하는 컨텐츠만 보임
+      tabContents[index].classList.add("show");
+
+      // 모든 탭 버튼 스타일을 초기화
+      tabButtons.forEach((btn) => {
+        btn.classList.remove("orange");
+      });
+
+      // 현재 클릭한 버튼에 스타일을 적용
+      button.classList.add("orange");
+    });
+  });
+
+  // 초기 상태 설정: 첫번쨰 탭을 활성화
+  tabButtons[0].click();
 });
 
 // jQuery //

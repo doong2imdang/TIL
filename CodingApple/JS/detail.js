f49606e1f6988e19d9ac89ecb125ae05648d4f6a
KIm Seo Young <jmh7367@naver.com>
2023-10-26 12:41:17 +0900

    9. 탭기능 만들며 배우는 for 반복문

---
+++ CodingApple/JS/detail.js
@@ -0,0 +1,15 @@
+// 버튼O 누르면
+// - 모든 버튼에 붙은 orange 클래스명 제거
+// - 버튼O에 orange 클래스명 추가
+// - 모든 div에 붙은 show 클래스명 제거
+// - divO에 show 클래스명 추가
+document.querySelector(".tab-button").addEventListener("click", function () {
+  // 숙제. 버튼 3개 기능 전부 숙제로 해오기
+});
+
+// jQuery //
+
+//     querySelectorAll('.tab-button')[0]과 비슷함
+// $(".tab-button")
+//   .eq(0)
+//   .on("click", function () {});

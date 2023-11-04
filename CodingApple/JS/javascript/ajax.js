c0578113044ea295a262290fa197d173fc32ccd6
KIm Seo Young <jmh7367@naver.com>
2023-11-04 23:25:54 +0900

    9. Ajax 1 - 개념정리_숙제2

--- CodingApple/JS/javascript/ajax.js
+++ CodingApple/JS/javascript/ajax.js
@@ -3,3 +3,15 @@ var products = [
   { id: 1, price: 50000, title: "Springfield Shirt" },
   { id: 2, price: 60000, title: "Black Monastery" },
 ];
+
+// 숙제. 상품목록 3개 만들어오기(자바스크립트로 html 생성하기)
+for (let i = 0; i < products.length; i++) {
+  var 템플릿 = `
+  <div class="col-sm-4">
+    <img src="https://via.placeholder.com/600" class="w-100" />
+      <h5>${products[i].title}</h5>
+      <p>가격 : ${products[i].price}원</p>
+    </div>
+  `;
+  document.querySelector(".row").insertAdjacentHTML("beforeend", 템플릿);
+}

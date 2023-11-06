941b735014babd65765bce75c24abd71145eb42c
KIm Seo Young <jmh7367@naver.com>
2023-11-06 17:20:00 +0900

    10. array에서 자주쓰는 sort, map, filter 함수

--- CodingApple/JS/javascript/ajax.js
+++ CodingApple/JS/javascript/ajax.js
@@ -121,3 +121,123 @@ document.querySelector("#more").addEventListener("click", function () {
       console.log(error);
     });
 });
+
+/// 10. array에 자주 쓰는 sort, map, filter 함수 ///
+// 가격순 정렬
+// products array를 정렬하고 products에 있던걸로 html 다시 생성하면 끝일듯
+
+// array 정렬하는 법
+// 문자 정렬은 array.sort()
+var 어레이 = [7, 3, 5, 2, 40];
+var 어레이2 = ["a", "b", "c"];
+
+어레이.sort();
+console.log(어레이); // [2, 3, 40, 5, 7]
+
+// 숫자 정렬은
+어레이.sort(function (a, b) {
+  return a - b;
+});
+console.log(어레이); // [2, 3, 5, 7, 40]
+
+// 동작원리????
+// 1. a, b 는 arrya 안의(에있던) 자료(들)임
+// 2. return 오른쪽이 양수면 a를 오른쪽으로
+// 3. return 오른쪽이 음수면 b를 오른쪽으로
+
+// 숫자내림차순 정렬은? (3, 2, 1순)
+어레이.sort(function (a, b) {
+  return b - a;
+});
+console.log(어레이); // [40, 7, 5, 3, 2]
+
+// 문자 가나다순 정렬은?
+어레이2.sort();
+console.log(어레이2); // ['a', 'b', 'c']
+
+// 숙제. 문자 다나가순 정렬은?
+어레이2.sort(function (a, b) {
+  if (a > b) {
+    return -1;
+  }
+  if (a < b) {
+    return 1;
+  } else {
+    return 0;
+  }
+});
+console.log(어레이2); // ['c', 'b', 'a']
+
+// Q. 버튼 누르면 products 안의 데이터를 가격순 정렬?
+var products = [
+  { id: 0, price: 70000, title: "Blossom Dress" },
+  { id: 1, price: 50000, title: "Springfield Shirt" },
+  { id: 2, price: 60000, title: "Black Monastery" },
+];
+
+document.querySelector("#price").addEventListener("click", function () {
+  products.sort(function (a, b) {
+    return a.price - b.price;
+  });
+
+  console.log(products);
+});
+
+// Q. 버튼 눌러도 왜 html은 안바뀜?
+// 컴퓨터는 여러분이 시키는것만 할뿐 명령을 내려야함
+
+document.querySelector("#price").addEventListener("click", function () {
+  products.sort(function (a, b) {
+    return a.price - b.price;
+  });
+
+  // 1. 카드 3개있던 div의 내용제거
+  document.querySelector(".row").innerHTML = "";
+
+  // 2. products 갯수만큼 카드 생성~ 정렬대로
+  products.forEach((data) => {
+    var 템플릿3 = `
+    <div class="col-sm-4">
+      <img src="https://via.placeholder.com/200" class="w-100" />
+      <h5>${data.title}</h5>
+      <p>가격 : ${data.price}원</p>
+    </div>
+    `;
+    document.querySelector(".row").insertAdjacentHTML("beforeend", 템플릿3);
+  });
+});
+
+// array 자료 원하는 것만 필터하려면 .filter() //
+// a => array에 있던 자료임
+// filter() 결과는 변수에 저장해서 써야함
+var 어레이 = [7, 3, 5, 2, 40];
+var 새어레이 = 어레이.filter(function (a) {
+  // return 조건식
+  return a < 4;
+});
+console.log(새어레이); // [3, 2]
+
+// array 자료 전부 변형하려면 .map() //
+// 달러표시 상품가격인데 전부 원화로 변경하려면?
+// 환율을 전체에 곱해주면 되는 그런곳에 활용함
+var 어레이 = [7, 3, 5, 2, 40];
+
+var 새어레이 = 어레이.map(function (a) {
+  return a * 4;
+});
+console.log(새어레이); // [28, 12, 20, 8, 160]
+
+// sort 와 filter 비교
+// .sort() 는 원본변형 O
+// .filter() 는 원본변형 X
+
+var products = [
+  { id: 0, price: 70000, title: "Blossom Dress" },
+  { id: 1, price: 50000, title: "Springfield Shirt" },
+  { id: 2, price: 60000, title: "Black Monastery" },
+];
+
+// 숙제1. '상품명 다나가순 정렬' 버튼과 기능
+
+// 숙제2. '6만원 이하 상품만 보기' 버튼과 기능
+// products에서 6만원 이하만 남기면 될듯 그리고 그걸로 html 새로 생성

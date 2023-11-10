017a7e90b33a629aba21dd5791b075215321b8a8
KIm Seo Young <jmh7367@naver.com>
2023-11-10 12:30:26 +0900

    3. 리액트에서 레이아웃 만들 때 쓰는 JSX 문법 3개

--- CodingApple/React/blog/src/App.js
+++ CodingApple/React/blog/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  // 대충 서버에서 가져온 실제 데이터임
+  let post = "강남 우동 맛집";
+  // document.querySelector('h4').innerHTML = post; (Javascript상에서)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={{ color: "red", fontSize: "16px" }}>블로그임</h4>
+      </div>
+      <h4>{post}</h4>
     </div>
   );
 }
 
 export default App;
+

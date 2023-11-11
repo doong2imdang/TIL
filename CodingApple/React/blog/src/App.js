3a9c2804da036de9bf42a93598efc6383428274c
KIm Seo Young <jmh7367@naver.com>
2023-11-11 11:55:05 +0900

    5. 버튼에 기능개발을 해보자 & 리액트 state 변경하는법

--- CodingApple/React/blog/src/App.js
+++ CodingApple/React/blog/src/App.js
@@ -1,3 +1,6 @@
+/* eslint-disable */
+// Lint 끄는 기능임
+
 import "./App.css";
 import { useState } from "react";
 
@@ -18,7 +21,21 @@ function App() {
   // Q. state 언제 써야함?
   // -> 변동시 자동으로 html 반영되게 만들고 싶으면 state 쓰셈
 
-  // 숙제. 이렇게 만들어오세요. 근데 제목들을 state로 만들어오셈
+  // 숙제. 이렇게 만들어오세요. 근데 제목들을 state로 만들어오셈(완)
+
+  let [따봉, 따봉변경] = useState(0);
+  // 따봉변경 => state 변경용 함수임
+
+  // onClick={안엔 함수이름을 넣어야함} => 함수 만드는 문법을 바로 넣어도 상관없음( () => {} or function() { })
+
+  // state 변경하는 법(등호로 변경금지)
+  // state변경함수(새로운state)
+
+  // 오늘 배운거
+  // 1. onClick 쓰는 법 => onClick={} 안엔 함수 넣어야함
+  // 2. state 변경하는 법(항상 state 변경함수 쓰세요) => state변경함수(새로운 state)
+
+  // 숙제. 버튼 누르면 첫 글이 '여자코트 추천'으로 바뀌는 기능만들기
   return (
     <div className="App">
       <div className="black-nav">
@@ -26,7 +43,17 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
       <div className="list">
-        <h4>{글제목[0]}</h4>
+        <h4>
+          {글제목[0]}{" "}
+          <span
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}
+          >
+            👍
+          </span>{" "}
+          {따봉}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">

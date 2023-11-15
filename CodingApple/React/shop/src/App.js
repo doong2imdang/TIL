89b9ac7def82220947b72b0cfde0f4946be3dbe0
KIm Seo Young <jmh7367@naver.com>
2023-11-15 15:35:16 +0900

    1. 새로운 프로젝트 생성 & Bootstrap 사용하기

---
+++ CodingApple/React/shop/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">ShoesShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
+

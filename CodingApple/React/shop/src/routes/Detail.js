51ed405736b3472ca14042634bbc231b17566f3d
KIm Seo Young <jmh7367@naver.com>
2023-12-01 15:45:52 +0900

    7. 리액트 라우터 3 - URL 파라미터로 상세페이지 100개 만들기_응용

--- CodingApple/React/shop/src/routes/Detail.js
+++ CodingApple/React/shop/src/routes/Detail.js
@@ -14,21 +14,26 @@ export default function Detail(props) {
 
   // 오늘의 응용 1. /detail/0 접속 시 0번째 상품말고 상품 id가 0인걸 보여주면 좋을듯(data.js에서 id 가져오면 될듯) => 왜냐? 만약에 home에서 가나다 순으로 정렬이 되면 Grey Yordan이 0번째가 되어버려서 순서가 꼬일 수 있음
   let { id } = useParams();
-  // console.log(id);
+  let 찾은상품 = props.shoes.find((x) => {
+    return x.id === parseInt(id);
+  });
+  console.log(찾은상품.id);
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6">
           <img
-            src={`https://codingapple1.github.io/shop/shoes${id}.jpg`}
+            src={`https://codingapple1.github.io/shop/shoes${
+              찾은상품.id + 1
+            }.jpg`}
             width="100%"
           />
         </div>
         <div className="col-md-t mt-4">
-          <h4 className="pt-5">{props.shoes[id].title}</h4>
-          <p>{props.shoes[id].content}</p>
-          <p>{props.shoes[id].price}원</p>
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}원</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>

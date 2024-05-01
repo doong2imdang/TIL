159c2a480fa30bedf42018984da9a3cd7514eedd
KIm Seo Young <jmh7367@naver.com>
2024-05-01 11:00:00 +0900

    4. 타입을 미리 정하기 애매할 때(union type, any, unknown)

--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -1,56 +1,53 @@
-// 변수만들기 //
+// Union Type (타입 2개 이상 합친 새로운 타입 만들기) //
 
-// 변수에 타입 지정 가능 -> 변수명 :타입명 = 변수에 실드 씌우는 것임
-// 타입에는 string, number, boolean, null, undefined ...
-let 이름: string = "kim";
-// 이름 = 123; (장점: 타입이 실수로 변경될 때 에러내줌)
-let 나이: number = 50;
-let 결혼했니: boolean = true; // boolean은 true/false 자료를 뜻함
-let 결혼결혼: null = null; // undefined, null 타입도 있습니다.
-let 미혼미혼: undefined = undefined;
+// 문자 or 숫자 들어올 수 있는 변수는 어떻게 만듭니까
+let 회원: number | string | boolean = 123;
+회원;
+// Q. 숫자 or 문자가 가능한 array / object 타입 지정은?
+let 회원들: (number | string)[] = [1, "2", 3];
+let 오브젝트: { a: string | number } = { a: 123 };
 
-// 변수 하나에 여러자료 집어넣고 싶으면 array 자료형 쓰면 됩니다.
-// array 자료에 타입 지정 가능
-let 회원들: string[] = ["kim", "park"]; // -> 문자만 담긴 array만 가능
-// Q. 저는 array에 숫자랑 문자 동시에 넣고 싶은데요?
-// A. 나중에 알아봅시다. Union type을 쓰면 됩니다.
+// any 타입 //
 
-// 변수 하나에 여러자료 집어넣고 싶으면 object 자료형 써도 가능
-// object 자료에도 타입 지정 가능
-let 회원s: { member1: string; member2: string } = {
-  member1: "kim",
-  member2: "park",
-}; // -> 이 변수엔 object만 들어올 수 있음
+// => 타입실드 해제문법임
+// 모든 자료형 허용해줌
+// 그럼 타입스크립트 쓰는 의미가 없는뎅
+// 일반 JS 변수로 만들고 싶으면 쓰셈
+// 그래서 타입 관련 버그가 나도 잡아주지 않음
+let 이름: any;
+이름 = 123;
+이름 = true;
+이름 = "kim";
+이름 = [];
 
-// 오늘의 pro tip
-// 타입스크립트 갓 배운 사람 특징_온갖 곳에 다 타입지정 해놓음
-// 타입 지정 원래 자동으로 됩니다. -> 타입 지정 문법 생략 가능
-let 회원들들 = "park";
-let 회원임 = 123;
-let 회원일까 = [1, 2, 3];
+// unknown 타입 //
 
-// JS 문법 그대로 TS에서 사용 가능
-// 저번에 셋팅해놓은 곳에 코드 짤 것임
-// - nodeJS 최신버전 설치
-// - 터미널에 npm install -g typescript
-// - tsconfig.json 만들고
-// - 어쩌구.ts 파일에 코딩 시작
-// - 터미널에 tsc -w 헤둔 채로
+// 모든 자료형 허용해줌
+// any 보다 안전한 이유는
+let 나이: unknown;
+나이 = 123;
+나이 = "kim";
+나이 = [];
+나이 = {};
+// let 변수1 :string = 나이; -> 지금 '나이'가 {}이기 때문에 에러가 생김
+// any로 될 경우 에러가 생기지 않음
 
-// Q1. 여려분의 이름, 나이, 출생지역을 변수로 각각 저장해봅시다.
-let myName: string = "doong";
-let myAge: number = 27;
-let placeOfBirth: string = "Ansan";
+// * 타입스크립트의 엄격함에 대해 * //
+// 나이 - 1 => 간단한 수학연산도 타입 맞아야함 (unknown타입은 뺄셈 안됨)
+// => unknown은 number 타입이 아님
+// (중요) 타입 엄격하게 지켜야함
 
-// Q2. 여러분이 가장 좋아하는 곡과 가수이름을 변수에 object 자료형으로 담아보십시오.
-let 마이훼이보릿: { 곡: string; 가수: string } = {
-  곡: "Golden Hour",
-  가수: "Ateez",
-};
+// Q. 왜 타입 맞는데 +1 안되는 것?
+let 연세: string | number;
+// 연세 + 1; (에러)
+// - 타입스크립트는 타입 엄격한거 좋아한다고 했잖음
+// string 타입 + 1 (허용)
+// number 타입 + 1 (허용)
+// string | number 타입 + 1 (안돼) / -1도 안돼
 
-// Q3. 다음과 같이 생긴 자료의 타입 지정을 해보도록 합시다.
-let project: { member: string[]; days: number; started: boolean } = {
-  member: ["kim", "park"],
-  days: 30,
-  started: true,
-};
+// Q. 변수가 분명 1인데 왜 -1이 안되는 것?
+let 춘추: unknown = 1;
+// 춘추 -1; (에러)
+// - 타입스크립트는 엄격한거 좋아한댔음
+// 숫자타입이여야 숫자처럼 연산해주죠
+// 나중에 Narrowing / Assertion 배워서 엄격하게 코드짜면 됩니다.

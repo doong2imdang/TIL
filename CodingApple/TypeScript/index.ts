3c67e605a9058b36eafe17b950f83ab1f0d36ad2
KIm Seo Young <jmh7367@naver.com>
2024-05-12 11:00:00 +0900

    6. 타입 확정하기 Narrowing & Assertion_숙제2

--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -80,3 +80,21 @@ function 클리닝(x: (number | string)[]) {
 }
 
 console.log(클리닝([123, "3"]));
+
+// (숙제 2) 다음과 같은 함수를 만들어보십시오.
+let 철수쌤 = { subject: "math" };
+let 영희쌤 = { subject: ["science", "english"] };
+let 민수쌤 = { subject: ["science", "art", "korean"] };
+// 지금 여러 변수에 선생님이 가르치고 있는 과목이 저장되어 있습니다. 과목 1개만 가르치는 쌤들은 문자 하나로 과목이 저장이 되었고, 과목 2개 이상 가르치는 쌤들은 array자료로 과목들이 저장되어 있습니다.
+// '철수쌤' 같은 object 자료를 파라미터로 집어넣으면 그 선생님이 가르치고 있는 과목 중 맨 뒤의 1개를 return 해주는 함수를 만들어봅시다. 그리고 타입지정도 엄격하게 해보도록 합시다.
+function 과목(x: { subject: string | string[] }) {
+  if (typeof x.subject === "string") {
+    return x.subject;
+  } else if (Array.isArray(x.subject)) {
+    return x.subject[x.subject.length - 1];
+  } else {
+    return "없어유";
+  }
+}
+
+console.log(과목({ subject: ["english", "art"] }));

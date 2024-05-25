fffd47506792f6d79a2a8290192b2d5345ed6204
KIm Seo Young <jmh7367@naver.com>
2024-05-25 23:33:17 +0900

    1.3. object 타입 지정

--- CodingApple/TypeScript/index.js
+++ CodingApple/TypeScript/index.js
@@ -4,5 +4,9 @@
 // "이 변수엔 string(문자) type만 들어올 수 있습니다"
 // string, number, boolean, null, undefined, bigint, [], {}, 등...
 // array 타입 지정 //
-var 이름 = ["kim", "park"];
+// let 이름: string[] = ["kim", "park"];
 // "이 변수엔 string 담긴 array만 들어올 수 있습니다."
+// object 타입 지정 //
+var 이름 = { name: "kim" };
+// 여기서 ?는 'name 속성은 옵션이에얌'
+// let 이름: { name?: string } = { }; 일때 에러가 나지 않도록 하는 것임

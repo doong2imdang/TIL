d5ca175ac733a6d65fb9c88670cc662cb6d7f143
KIm Seo Young <jmh7367@naver.com>
2024-05-25 23:30:27 +0900

    1.2. array 타입 지정

--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -1,5 +1,9 @@
 // 간단한 변수 타입 지정 가능 //
-let 이름: string = "kim";
+// let 이름: string = "kim";
 // 이름 = 123; -> 'number'형식은 'string'형식에 할당할 수 없습니다 에러
 // "이 변수엔 string(문자) type만 들어올 수 있습니다"
 // string, number, boolean, null, undefined, bigint, [], {}, 등...
+
+// array 타입 지정 //
+let 이름: string[] = ["kim", "park"];
+// "이 변수엔 string 담긴 array만 들어올 수 있습니다."

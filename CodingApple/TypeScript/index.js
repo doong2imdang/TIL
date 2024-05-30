4aa925fae3b39ae7b7f58fc28c1a9d47c1ef8d6e
KIm Seo Young <jmh7367@naver.com>
2024-05-30 11:43:52 +0900

    8. Literal Types로 만드는 const 변수 유사품

--- CodingApple/TypeScript/index.js
+++ CodingApple/TypeScript/index.js
@@ -1,32 +1,47 @@
-// type alias(별칭 => 변수) 만드는 법 //
-var 동물 = "kim";
-var 동물s = { name: "kim", age: 20 };
-// const 변수 잠깐 설명시간
-var 출생지역 = "seoul";
-// 출생지역 = 'busan'; // 에러
-var 출생지역s = { region: "seoul" };
-출생지역s.region = "busan";
-var 여친 = {
-    name: "엠버",
-};
-var 여친s = {
-    name: "엠버",
-};
-var position = { x: 10, y: 20 }; // 에러 안나는거 보니 잘 되는군
-var substitute = { x: 10, y: "10" }; // 오류 없음
-var example1 = {
-    color: "green",
-    size: 20,
-    position: [90, 100, 105],
-};
-var person1 = {
+// Literal type //
+// - 변수에 뭐가 들어올지 더 엄격하게 관리가능
+// - 자동완성 힌트 굿
+// 더 엄격한 타입지정 가능
+// 'kim or park 이런 문자만 들어올 수 있습니다~'
+// let 이름: string;
+// =>
+var 이름;
+이름 = 123;
+// 이름 = 456; 은 에러
+var 접니다;
+접니다 = "대머리";
+접니다 = "솔로";
+// 함수도 되나 테스트 해봅시다
+// 함수도 값을 지정할 수도 있다.
+function 함수(a) {
+    return 1;
+}
+함수("hello");
+// Q. 이런 함수 어떻게 만들죠?
+// 1. 가위 or 바위 or 보 중 1개 입력가능 // 함수('가위')
+// 2. 가위 or 바위 or 보 만 들어올 수 있는 array를 return 해야함 // ['가위', '보']
+function 가위바위보(x) {
+    return ["가위"];
+}
+가위바위보("가위");
+// const 변수의 한계 //
+// Literal type은 const 변수와 유사하게 사용가능
+// Literal type은 const 변수 업글버전임
+// Literal type 의 문제점 //
+var 자료 = {
     name: "kim",
-    phone: 123123123,
-    email: "abc@naver.com",
 };
-var person2 = {
-    name: "lee",
-    phone: 123123123,
-    email: "example@naver.com",
-    minorstatus: true,
-};
+자료.name; // 'kim'
+function 내함수(a) { }
+내함수("kim");
+// 내함수(자료.name)   // error
+// 파라미터를 저렇게 설정한게 'kim'이라는 자료만 들어올 수 있습니다 가 아님
+// => 파라미터에 'kim'이라는 타입만 들어올 수 있습니다임
+// 내함수(자료.name)에 들어올 수 있는 타입은 'kim : string'인데 자료.name:string이 들어와서 안되는 것임
+// 솔루션 1. object 만들 때 타입지정 확실히 하셈
+// 솔루션 2. as 문법으로 타입을 구라치든가
+// 솔루션 3. as const 이상한 키워드 쓰든가 => 이 object는 literal type 지정 알아서 해주셈
+// 효과 1. object value 값을 그대로 타입으로 지정해줌
+// 효과 2. object 속성들에 모두 readonly 붙여줌
+// 예시) 자료.name = 123;으로 하면 에러
+// object 자료를 완전히 잠가놓고 싶으면 as const 써보셈
--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -1,120 +1,59 @@
-// type alias(별칭 => 변수) 만드는 법 //
+// Literal type //
+// - 변수에 뭐가 들어올지 더 엄격하게 관리가능
+// - 자동완성 힌트 굿
 
-// type 변수 작명 관습
-// - 시작은 영어대문자 국룰
-// - 뒤에 Type 붙여주기(선택)
+// 더 엄격한 타입지정 가능
+// 'kim or park 이런 문자만 들어올 수 있습니다~'
 
-type AnimalType = string | number | undefined;
-let 동물: AnimalType = "kim";
+// let 이름: string;
+// =>
+let 이름: 123;
+이름 = 123;
+// 이름 = 456; 은 에러
 
-type AnimalsType = { name: string; age: number };
-let 동물s: AnimalsType = { name: "kim", age: 20 };
+let 접니다: "대머리" | "솔로";
+접니다 = "대머리";
+접니다 = "솔로";
 
-// const 변수 잠깐 설명시간
-const 출생지역 = "seoul";
-// 출생지역 = 'busan'; // 에러
+// 함수도 되나 테스트 해봅시다
+// 함수도 값을 지정할 수도 있다.
+function 함수(a: "hello"): 1 | 0 {
+  return 1;
+}
+함수("hello");
 
-const 출생지역s = { region: "seoul" };
-출생지역s.region = "busan";
-// const 는 재할당을 금지하는 키워드지 안에 있는 object 수정을 금지하는 것은 아님
-// => const 변수는 등호로 재할당만 막는 역할임
-// => const로 담은 object 수정은 자유롭게 가능
+// Q. 이런 함수 어떻게 만들죠?
+// 1. 가위 or 바위 or 보 중 1개 입력가능 // 함수('가위')
+// 2. 가위 or 바위 or 보 만 들어올 수 있는 array를 return 해야함 // ['가위', '보']
+function 가위바위보(x: "가위" | "바위" | "보"): ("가위" | "바위" | "보")[] {
+  return ["가위"];
+}
+가위바위보("가위");
 
-// typescript 쓰면 object 자료 수정도 막을 수 있음(readonly) //
-type Girlfriend = {
-  readonly name: string;
-};
-const 여친: Girlfriend = {
-  name: "엠버",
-};
+// const 변수의 한계 //
 
-// 여친.name = "유라"; // readonly를 붙이면 에러가 생김 => 실수로 수정하면 에러내줌
-// => readonly 쓰면 object 자료 수정도 막을 수 있음
-// (주의) 타입스크립트 에러는 에디터 & 터미널에서만 존재함
-// 실제 변환된 js 파일은 에러없음
+// Literal type은 const 변수와 유사하게 사용가능
+// Literal type은 const 변수 업글버전임
 
-// object 속성 안에도 ? 사용가능
-type Girlfriends = {
-  name?: string; // name 속성은 선택 사항임(? -> string | undefined 가 들어올 수 있음)
-};
-const 여친s: Girlfriends = {
-  name: "엠버",
-};
-
-// type 변수 당연히 union type으로 extend(합치기) 가능 //
-// 나중에 interface 키워드 배울 때 다시 다룰 듯
-type Name = string;
-type Age = number;
-type Person = Name | Age;
-
-// & 연산자로 object 타입 합치기 //
-type PositionX = { x: number };
-type PositionY = { y: number };
-// {x : number, y : number} => 이걸 만들고 싶어~~
-
-type NewType = PositionX & PositionY; // => {x : number, y : number} 만들어짐
-
-let position: NewType = { x: 10, y: 20 }; // 에러 안나는거 보니 잘 되는군
-
-// (참고) 같은 이름의 type 변수 재정의 불가능 //
-type 어쩌구 = { x: number };
-// type 어쩌구 = number; => 에러생김
-
-// (숙제 1) object 타입을 정의한 type alias 두개를 & 기호로 합칠 때 중복된 속성이 있으면 어떻게 될까요?
-type OneType = { x: number; y: string };
-type TwoType = { x: number };
-type OnePlusTwo = OneType & TwoType;
-
-let substitute: OnePlusTwo = { x: 10, y: "10" }; // 오류 없음
-// 'x' 속성이 두 타입 모두에서 'number' 타입으로 일치하기 때문에 에러없이 수행할 수 있음
-
-type ThreeType = { x: number; y: string };
-type FourType = { x: string };
-type ThreePlusFour = ThreeType & FourType;
-
-// let substitution: ThreePlusFour = { x: 10, y: "10" };  // 오류 발생
-// 하지만 'x' 속성이 다른 타입이었다면, 예를 들어 'string'이었다면 위와 같은 오류가 발생합니다. => 이 경우 타입스크립트는 'x' 속성이 'number'와 'string' 타입을 동시에 만족할 수 없기때문에 오류를 발생시킵니다.
-// 따라서 두 타입을 '&' 기호로 합칠 때, 중복된 속성의 타입이 동일하면 문제가 없으며, 타입이 다르면 타입스크립트에서 오류가 발생하게 됩니다.
-
-// (숙제 2) 다음 조건을 만족하는 타입을 만들어봅시다.
-// 1. 이 타입은 object 자료형이어야합니다.
-// 2. 이 타입은 color라는 속성을 가질 수 있으며 항상 문자가 들어와야합니다.
-// 3. 이 타입은 size 라는 속성이 있어야하며 항상 숫자가 들어와야합니다.
-// 4. 이 타입은 position이라는 변경불가능한 속성이 있어야하며 항상 숫자가 담긴 array 자료가 들어와야 합니다.
-// type alias로 만들어보셈.
-type ColorType = { color: string };
-type SizeType = { size: number };
-type PositionType = { readonly position: number[] };
-
-type CustomType = ColorType & SizeType & PositionType;
-
-const example1: CustomType = {
-  color: "green",
-  size: 20,
-  position: [90, 100, 105],
-};
-
-// (숙제 3) 다음을 만족하는 type alias를 연습삼아 간단히 만들어보십시오.
-// 1. 대충 이렇게 생긴 object 자료를 다룰 일이 많습니다.
-// {name : 'kim', phone : 123, email: 'abc@naver.com'};
-// 2. object 안에 있는 이름, 전화번호, 이메일 속성이 옳은 타입인지 검사하는 type alias를 만들어봅시다.
-// 3. 각 속성이 어떤 타입일지는 자유롭게 정하십시오.
-type InfoType = { name: string; phone: number; email: string };
-const person1: InfoType = {
+// Literal type 의 문제점 //
+var 자료 = {
   name: "kim",
-  phone: 123123123,
-  email: "abc@naver.com",
-};
+} as const;
+자료.name; // 'kim'
 
-// (숙제 4) 다음을 만족하는 type alias를 만들어보십시오.
-// 1. 숙제2와 똑같은데 이번엔 이름, 전화번호, 이메일, 미성년자여부 속성을 옳은 타입인지 검사하는 type alias를 만들어봅시다.
-// 2. 미성년자 여부 속성은 true/false만 들어올 수 있습니다.
-// 3. 멋있게 숙제 3에서 만들어둔 type alias를 재활용해봅시다.
-type MinorstatusType = { minorstatus: boolean };
-type PersonInfo = InfoType & MinorstatusType;
-const person2 = {
-  name: "lee",
-  phone: 123123123,
-  email: "example@naver.com",
-  minorstatus: true,
-};
+function 내함수(a: "kim") {}
+내함수("kim");
+// 내함수(자료.name)   // error
+// 파라미터를 저렇게 설정한게 'kim'이라는 자료만 들어올 수 있습니다 가 아님
+// => 파라미터에 'kim'이라는 타입만 들어올 수 있습니다임
+// 내함수(자료.name)에 들어올 수 있는 타입은 'kim : string'인데 자료.name:string이 들어와서 안되는 것임
+
+// 솔루션 1. object 만들 때 타입지정 확실히 하셈
+// 솔루션 2. as 문법으로 타입을 구라치든가
+
+// 솔루션 3. as const 이상한 키워드 쓰든가 => 이 object는 literal type 지정 알아서 해주셈
+// 효과 1. object value 값을 그대로 타입으로 지정해줌
+// 효과 2. object 속성들에 모두 readonly 붙여줌
+// 예시) 자료.name = 123;으로 하면 에러
+
+// object 자료를 완전히 잠가놓고 싶으면 as const 써보셈

c35c16b9a0e2d3f8651aab8d1c8a27230e4a75d4
KIm Seo Young <jmh7367@naver.com>
2024-09-01 22:08:07 +0900

    Next.js 1-9. 좋아요 버튼 만들기 (useState, onClick)

--- CodingApple/Next.js/fresh/app/list/page.js
+++ CodingApple/Next.js/fresh/app/list/page.js
@@ -1,8 +1,10 @@
-// import Image from "next/image";
-import Tomatoes from "/public/food0.png";
+"use client";
+
+import { useState } from "react";
 
 export default function List() {
   let 상품 = ["Tomatoes", "Pasta", "Coconut"];
+  const [수량, 수량변경] = useState(0);
 
   return (
     <div>
@@ -12,6 +14,14 @@ export default function List() {
           <div className="food" key={index}>
             <img src={`/food${index}.png`} className="food-img" />
             <h4>{item} $40</h4>
+            <span> {수량} </span>
+            <button
+              onClick={() => {
+                수량변경(수량 + 1);
+              }}
+            >
+              +
+            </button>
           </div>
         );
       })}

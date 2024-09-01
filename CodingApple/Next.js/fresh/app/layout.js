3bbd2310c55b07acabce7ab5ea42e5d95233ddcc
KIm Seo Young <jmh7367@naver.com>
2024-09-01 21:00:24 +0900

    Next.js 1-6. Next.js에서 이미지 넣는 법 2개

--- CodingApple/Next.js/fresh/app/layout.js
+++ CodingApple/Next.js/fresh/app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
         <div className="navbar">
           <Link href="/">home</Link>
           <Link href="/list">list페이지</Link>
+          <Link href="/cart">cart페이지</Link>
         </div>
         {children}
       </body>
--- CodingApple/Next.js/fresh/app/list/page.js
+++ CodingApple/Next.js/fresh/app/list/page.js
@@ -1,3 +1,6 @@
+// import Image from "next/image";
+import Tomatoes from "/public/food0.png";
+
 export default function List() {
   let 상품 = ["Tomatoes", "Pasta", "Coconut"];
 
@@ -7,6 +10,7 @@ export default function List() {
       {상품.map((item, index) => {
         return (
           <div className="food" key={index}>
+            <img src={`/food${index}.png`} className="food-img" />
             <h4>{item} $40</h4>
           </div>
         );

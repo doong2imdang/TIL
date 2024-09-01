8b0abc562ccb8aa897213d2bc78175fd98db464c
KIm Seo Young <jmh7367@naver.com>
2024-09-01 11:06:54 +0900

    Next.js 1-4. 여러 페이지 만들기(라우팅)

--- CodingApple/Next.js/fresh/app/page.js
+++ CodingApple/Next.js/fresh/app/page.js
@@ -1,15 +1,9 @@
 export default function Home() {
-  let name = "kim";
-  let link = "https://naver.com";
-
   return (
-    <div>
-      <h4 className="title" style={{ color: "red", fontSize: "30px" }}>
-        애플후레시
-      </h4>
-      <p className="title-sub">by dev {name}</p>
-      <a href={link}>링크</a>
-    </div>
+    <main>
+      <h4 className="title">애플후레시</h4>
+      <p className="title-sub">by dev kim</p>
+    </main>
   );
 }
 

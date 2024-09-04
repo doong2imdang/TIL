4a7e415e83b4a21cdfa8db4c20a103cfd9e5193d
KIm Seo Young <jmh7367@naver.com>
2024-09-04 16:39:28 +0900

    Next.js 2-5. 상세페이지 만들기 2 (useRouter)

--- CodingApple/Next.js/forum/app/list/page.js
+++ CodingApple/Next.js/forum/app/list/page.js
@@ -1,5 +1,6 @@
 import { connectDB } from "@/util/database";
 import Link from "next/link";
+import DetailLink from "./DetailLink";
 
 export default async function List() {
   const client = await connectDB;
@@ -12,9 +13,10 @@ export default async function List() {
       {result.map((a, i) => {
         return (
           <div className="list-item" key={i}>
-            <Link href={"/detail/" + a["_id"]}>
+            <Link prefetch={false} href={"/detail/" + a["_id"]}>
               <h4>{a.title}</h4>
             </Link>
+            <DetailLink />
             <p>1월 1일</p>
           </div>
         );

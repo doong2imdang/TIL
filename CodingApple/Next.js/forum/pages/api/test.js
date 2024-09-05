957393cffeb1b7d974468e87050ca192f9d3b152
KIm Seo Young <jmh7367@naver.com>
2024-09-05 11:04:22 +0900

    Next.js 2-6-1. 글 작성기능 만들기 1(서버기능 개발은) 숙제 1

--- CodingApple/Next.js/forum/pages/api/test.js
+++ CodingApple/Next.js/forum/pages/api/test.js
@@ -1,5 +1,14 @@
-export default function handler(요청, 응답) {
+import { connectDB } from "@/util/database";
+
+export default async function handler(요청, 응답) {
+  const client = await connectDB;
+  const db = client.db("forum");
+  let result = await db.collection("post").find().toArray();
+  console.log(result);
+
   if (요청.method == "POST") {
     return 응답.status(200).json("처리완료");
+  } else if (요청.method == "GET") {
+    return 응답.status(200).json(result);
   }
 }

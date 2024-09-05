9c319323ccbbfcb33e3c6a74642e089ce753032b
KIm Seo Young <jmh7367@naver.com>
2024-09-05 11:08:12 +0900

    Next.js 2-6-2. 글 작성기능 만들기 1(서버기능 개발은) 숙제 2

--- CodingApple/Next.js/forum/pages/api/test.js
+++ CodingApple/Next.js/forum/pages/api/test.js
@@ -6,9 +6,11 @@ export default async function handler(요청, 응답) {
   let result = await db.collection("post").find().toArray();
   console.log(result);
 
+  const currentDate = new Date();
+
   if (요청.method == "POST") {
     return 응답.status(200).json("처리완료");
   } else if (요청.method == "GET") {
-    return 응답.status(200).json(result);
+    return 응답.status(200).json(currentDate);
   }
 }

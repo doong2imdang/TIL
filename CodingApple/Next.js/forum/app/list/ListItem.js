3ad829774ea838214da06562d086a5324c1ded0f
KIm Seo Young <jmh7367@naver.com>
2024-09-06 12:35:29 +0900

    Next.js 2-12. 삭제기능 만들기 3(query string & URL parameter)

--- CodingApple/Next.js/forum/app/list/ListItem.js
+++ CodingApple/Next.js/forum/app/list/ListItem.js
@@ -15,25 +15,33 @@ export default function ListItem({ result }) {
             <Link href={"/edit/" + a["_id"]}>✏️</Link>
 
             <span
-              onClick={() => {
-                fetch("/api/post/delete", {
-                  method: "DELETE",
-                  body: a._id,
-                })
-                  .then((response) => {
-                    if (response.status == 200) {
-                      return response.json();
-                    } else {
-                      // 서버가 에러코드 전송 시 실행할 코드
-                    }
-                  })
-                  .then((result) => {
-                    // 성공시 실행할 코드
-                  })
-                  .catch((error) => {
-                    // 인터넷문제로 실패시 실행할 코드
-                    console.log(error);
-                  });
+              onClick={(e) => {
+                // fetch("/api/post/delete", {
+                //   method: "DELETE",
+                //   body: a._id,
+                // })
+                //   .then((response) => {
+                //     if (response.status == 200) {
+                //       return response.json();
+                //     } else {
+                //       // 서버가 에러코드 전송 시 실행할 코드
+                //     }
+                //   })
+                //   .then((result) => {
+                //     // 성공시 실행할 코드
+                //     e.target.parentElement.style.opacity = 0;
+                //     setTimeout(() => {
+                //       e.target.parentElement.style.display = "none";
+                //     }, 1000);
+                //   })
+                //   .catch((error) => {
+                //     // 인터넷문제로 실패시 실행할 코드
+                //     console.log(error);
+                //   });
+                // query string
+                // fetch("/api/test?name=kim&age=20");
+
+                fetch("/api/abc/kim");
               }}
             >
               🗑️
--- CodingApple/Next.js/forum/pages/api/test.js
+++ CodingApple/Next.js/forum/pages/api/test.js
@@ -1,16 +1,19 @@
 import { connectDB } from "@/util/database";
 
 export default async function handler(요청, 응답) {
-  const client = await connectDB;
-  const db = client.db("forum");
-  let result = await db.collection("post").find().toArray();
-  console.log(result);
+  // const client = await connectDB;
+  // const db = client.db("forum");
+  // let result = await db.collection("post").find().toArray();
+  // console.log(result);
 
-  const currentDate = new Date();
+  // const currentDate = new Date();
 
-  if (요청.method == "POST") {
-    return 응답.status(200).json("처리완료");
-  } else if (요청.method == "GET") {
-    return 응답.status(200).json(currentDate);
-  }
+  // if (요청.method == "POST") {
+  //   return 응답.status(200).json("처리완료");
+  // } else if (요청.method == "GET") {
+  //   return 응답.status(200).json(currentDate);
+  // }
+
+  console.log(요청.query);
+  return 응답.status(200).json();
 }

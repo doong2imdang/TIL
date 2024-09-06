18e127d0fdb1b74ede3e016ffe596f98a5d67949
KIm Seo Young <jmh7367@naver.com>
2024-09-06 12:55:04 +0900

    Next.js 2-12-2. 삭제기능 만들기 3(query string & URL parameter) 응용 URL parameter로

--- CodingApple/Next.js/forum/pages/api/abc/[어쩌구].js
+++ CodingApple/Next.js/forum/pages/api/abc/[어쩌구].js
@@ -1,4 +1,14 @@
-export default function handler(요청, 응답) {
+import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
+
+export default async function handler(요청, 응답) {
   console.log(요청.query);
-  return 응답.status(200).json();
+
+  if (요청.method == "GET") {
+    const db = (await connectDB).db("forum");
+    let result = await db
+      .collection("post")
+      .deleteOne({ _id: new ObjectId(요청.query.어쩌구) });
+    응답.status(200).json("삭제완료");
+  }
 }
--- CodingApple/Next.js/forum/pages/api/post/delete.js
+++ CodingApple/Next.js/forum/pages/api/post/delete.js
@@ -10,12 +10,12 @@ export default async function handler(요청, 응답) {
   //   console.log(result);
   //   응답.status(200).json("삭제완료");
   // }
-
-  if (요청.method == "GET") {
-    const db = (await connectDB).db("forum");
-    let result = await db
-      .collection("post")
-      .deleteOne({ _id: new ObjectId(요청.query._id) });
-    응답.status(200).json("삭제완료");
-  }
+  // query string
+  // if (요청.method == "GET") {
+  //   const db = (await connectDB).db("forum");
+  //   let result = await db
+  //     .collection("post")
+  //     .deleteOne({ _id: new ObjectId(요청.query._id) });
+  //   응답.status(200).json("삭제완료");
+  // }
 }

00faf07ad8e5c5b50ab37805ad77638bced90636
KIm Seo Young <jmh7367@naver.com>
2024-09-09 13:02:06 +0900

    Next.js 2-17-3. 회원 기능 만들기 아이디, 비번 + JWT 사용하기 응용 3

--- CodingApple/Next.js/forum/app/write/page.js
+++ CodingApple/Next.js/forum/app/write/page.js
@@ -1,20 +1,26 @@
-export default function Write() {
-  // if(유저세션출력해서 아무것도 없으면)  {
-  //   return (
-  //     <div>
-  //       로그인하세요
-  //     </div>
-  //   )
-  // }
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+
+export default async function Write() {
+  let session = await getServerSession(authOptions);
+  console.log(session);
 
   return (
     <div className="p-20">
       <h4>글작성</h4>
-      <form action="/api/post/new" method="POST">
-        <input name="title" placeholder="글제목" />
-        <input name="content" placeholder="글내용" />
-        <button type="submit">버튼</button>
-      </form>
+      {session ? (
+        <>
+          <form action="/api/post/new" method="POST">
+            <input name="title" placeholder="글제목" />
+            <input name="content" placeholder="글내용" />
+            <button type="submit">버튼</button>
+          </form>
+        </>
+      ) : (
+        <>
+          <div>로그인하세요.</div>
+        </>
+      )}
     </div>
   );
 }

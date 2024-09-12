f86c714536dac95286e695a65b2b855aa426d191
KIm Seo Young <jmh7367@naver.com>
2024-09-12 22:31:31 +0900

    Firebase 4.2. Tweeting to Firestore

--- NomadCoders/Firebase/nwitter-reloaded/src/firebase.ts
+++ NomadCoders/Firebase/nwitter-reloaded/src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCGPGTCGL2HCmW7kXRyXI5MyOS9KeoQ_9Q",
@@ -13,3 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);

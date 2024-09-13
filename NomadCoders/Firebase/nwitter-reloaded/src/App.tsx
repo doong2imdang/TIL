ac6b4f2240b2236dc320d0f41acec7f95a392c75
KIm Seo Young <jmh7367@naver.com>
2024-09-13 16:03:32 +0900

    Firebase 4.4. Fetching Timeline

--- NomadCoders/Firebase/nwitter-reloaded/src/App.tsx
+++ NomadCoders/Firebase/nwitter-reloaded/src/App.tsx
@@ -43,13 +43,17 @@ const router = createBrowserRouter([
 const GlobalStyles = createGlobalStyle`
 ${reset};
 
-* {
+  * {
   box-sizing: border-box;
-}
+  }
   body{
     background-color: black;
     color: white;
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif,
+  }
+
+  ::-webkit-scrollbar {
+    display:none;
   }
 `;
 

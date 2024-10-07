32662d6bd8600e8ac5ab35476ef939d3ce9daa08
KIm Seo Young <jmh7367@naver.com>
2024-10-07 14:55:04 +0900

    TypeScript 1.5. 함수에 타입 지정하는 법 & void 타입 숙제 1

--- CodingApple/TypeScript/index.js
+++ CodingApple/TypeScript/index.js
@@ -1,5 +1,9 @@
-function 함수(x) {
-    if (typeof x === "number") {
-        console.log(x + 3);
+function 함수(name) {
+    if (name) {
+        console.log("안녕하세요" + name);
+    }
+    else {
+        console.log("이름이 없습니다.");
     }
 }
+함수("어쩌구");

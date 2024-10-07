5dec69ec4e16cbdfa72ceaa787a5bb4d0c9eecc6
KIm Seo Young <jmh7367@naver.com>
2024-10-07 12:51:44 +0900

    TypeScript 1.4. 타입을 미리 정하기 애매할 때 (union type, any, unknown)

--- CodingApple/TypeScript/index.js
+++ CodingApple/TypeScript/index.js
@@ -1,5 +1,7 @@
-var project = {
-    member: ["kim", "park"],
-    days: 30,
-    started: true,
-};
+var 회원들 = [1, "2", 3];
+var 오브젝트 = { a: 123 };
+var 나이;
+// 나이 + 1;
+if (typeof 나이 === "number") {
+    나이 + 1;
+}
--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -1,5 +1,8 @@
-let project: { member: string[]; days: number; started: boolean } = {
-  member: ["kim", "park"],
-  days: 30,
-  started: true,
-};
+let 회원들: (number | string)[] = [1, "2", 3];
+let 오브젝트: { a: string | number } = { a: 123 };
+
+let 나이: string | number;
+// 나이 + 1;
+if (typeof 나이 === "number") {
+  나이 + 1;
+}

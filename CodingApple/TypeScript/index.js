9d382fffe0176f4f1e80867cccb779594441bc16
KIm Seo Young <jmh7367@naver.com>
2024-10-07 16:57:04 +0900

    TypeScript 1.6. 타입 확정하기 Narrowing & Assertion 숙제 2

--- CodingApple/TypeScript/index.js
+++ CodingApple/TypeScript/index.js
@@ -1,13 +1,15 @@
-function 함수(a) {
-    var array = [];
-    a.forEach(function (e) {
-        if (typeof e === "string") {
-            array.push(parseInt(e));
-        }
-        else {
-            array.push(e);
-        }
-    });
-    return array;
+var 철수쌤 = { subject: "math" };
+var 영희쌤 = { subject: ["science", "english"] };
+var 민수쌤 = { subject: ["science", "art", "korean"] };
+function 함수(x) {
+    if (typeof x.subject === "string") {
+        return x.subject;
+    }
+    else if (Array.isArray(x.subject)) {
+        return x.subject[x.subject.length - 1];
+    }
+    else {
+        return "오류임";
+    }
 }
-console.log(함수(["1", 2, "3"]));
+console.log(함수({ subject: ["science", "art", "korean"] }));
--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -1,13 +1,15 @@
-function 함수(a: (number | string)[]) {
-  let array: number[] = [];
-  a.forEach((e) => {
-    if (typeof e === "string") {
-      array.push(parseInt(e));
-    } else {
-      array.push(e);
-    }
-  });
-  return array;
+let 철수쌤 = { subject: "math" };
+let 영희쌤 = { subject: ["science", "english"] };
+let 민수쌤 = { subject: ["science", "art", "korean"] };
+
+function 함수(x: { subject: string | string[] }) {
+  if (typeof x.subject === "string") {
+    return x.subject;
+  } else if (Array.isArray(x.subject)) {
+    return x.subject[x.subject.length - 1];
+  } else {
+    return "오류임";
+  }
 }
 
-console.log(함수(["1", 2, "3"]));
+console.log(함수({ subject: ["science", "art", "korean"] }));

c7f04559e34ea79b326afb5fa57d5d7bff518ef0
KIm Seo Young <jmh7367@naver.com>
2024-10-07 18:15:05 +0900

    TypeScript 1.7. 타입도 변수에 담아쓰세요 type 키워드 써서 & readonly 숙제 3

--- CodingApple/TypeScript/index.js
+++ CodingApple/TypeScript/index.js
@@ -2,4 +2,5 @@ var 회원가입정보 = {
     name: "kim",
     phone: 123,
     email: "a123@asdf.com",
+    status: true,
 };
--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -4,8 +4,15 @@ type User = {
   email: string;
 };
 
-let 회원가입정보: User = {
+type Status = {
+  status: boolean;
+};
+
+type UserInfo = User & Status;
+
+let 회원가입정보: UserInfo = {
   name: "kim",
   phone: 123,
   email: "a123@asdf.com",
+  status: true,
 };

8a9dbe3fcee29de52e4cfcd8dcd4d89dd2530044
KIm Seo Young <jmh7367@naver.com>
2024-10-07 18:01:58 +0900

    TypeScript 1.7. 타입도 변수에 담아쓰세요 type 키워드 써서 & readonly

--- CodingApple/TypeScript/index.js
+++ CodingApple/TypeScript/index.js
@@ -1,15 +1,1 @@
-var 철수쌤 = { subject: "math" };
-var 영희쌤 = { subject: ["science", "english"] };
-var 민수쌤 = { subject: ["science", "art", "korean"] };
-function 함수(x) {
-    if (typeof x.subject === "string") {
-        return x.subject;
-    }
-    else if (Array.isArray(x.subject)) {
-        return x.subject[x.subject.length - 1];
-    }
-    else {
-        return "오류임";
-    }
-}
-console.log(함수({ subject: ["science", "art", "korean"] }));
+var position = { x: 10, y: 20 };
--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -1,15 +1,6 @@
-let 철수쌤 = { subject: "math" };
-let 영희쌤 = { subject: ["science", "english"] };
-let 민수쌤 = { subject: ["science", "art", "korean"] };
+type PositionX = { x: number };
+type PositionY = { y: number };
 
-function 함수(x: { subject: string | string[] }) {
-  if (typeof x.subject === "string") {
-    return x.subject;
-  } else if (Array.isArray(x.subject)) {
-    return x.subject[x.subject.length - 1];
-  } else {
-    return "오류임";
-  }
-}
+type NewType = PositionX & PositionY;
 
-console.log(함수({ subject: ["science", "art", "korean"] }));
+let position: NewType = { x: 10, y: 20 };

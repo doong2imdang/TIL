3deb04a71898cc1f9157fa56096f3b7831ce265f
KIm Seo Young <jmh7367@naver.com>
2024-10-08 11:50:51 +0900

    TypeScript 1.9. 함수와 methods에 type alias 지정하는 법 숙제1

--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -1,9 +1,20 @@
-let 회원정보 = {
-  name: "kim",
-  plusOne(a: number): number {
-    return a + 1;
-  },
-  changeName: () => {},
+type Member = {
+  name: string;
+  age: number;
+  plusOne: (x: number) => number;
+  changeName: () => void;
 };
 
-회원정보.plusOne(2);
+let 회원정보: Member = {
+  name: "kim",
+  age: 30,
+  plusOne(x) {
+    return x + 1;
+  },
+  changeName: () => {
+    console.log("안녕");
+  },
+};
+
+회원정보.plusOne(1);
+회원정보.changeName();

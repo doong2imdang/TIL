7ede9eef389ee56b88d81254dbebaf1162fbe2e3
KIm Seo Young <jmh7367@naver.com>
2024-10-08 15:36:21 +0900

    TypeScript 1.13. class 만들 때 타입지정 가능

--- CodingApple/TypeScript/index.js
+++ CodingApple/TypeScript/index.js
@@ -1,17 +1,12 @@
-var 제목 = document.querySelector("#title");
-if ((제목 === null || 제목 === void 0 ? void 0 : 제목.innerHTML) != undefined) {
-    제목.innerHTML = "반가워요";
-}
-var 링크 = document.querySelectorAll(".naver");
-링크.forEach(function (e) {
-    if (e instanceof HTMLAnchorElement) {
-        e.href = "https://kakao.com";
+var Person = /** @class */ (function () {
+    function Person(a) {
+        this.name = a;
     }
-});
-var 버튼 = document.querySelector("#button");
-var 이미지 = document.querySelector("#image");
-버튼 === null || 버튼 === void 0 ? void 0 : 버튼.addEventListener("click", function () {
-    if (이미지 instanceof HTMLImageElement) {
-        이미지.src = "new.jpg";
-    }
-});
+    Person.prototype.함수 = function (a) {
+        console.log("안녕" + a);
+    };
+    return Person;
+}());
+var 사람1 = new Person("kim");
+var 사람2 = new Person("park");
+console.log(사람1.함수("어쩌구"));
--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -1,20 +1,15 @@
-let 제목 = document.querySelector("#title");
-if (제목?.innerHTML != undefined) {
-  제목.innerHTML = "반가워요";
+class Person {
+  name: string;
+  constructor(a: string) {
+    this.name = a;
+  }
+
+  함수(a: string) {
+    console.log("안녕" + a);
+  }
 }
 
-let 링크 = document.querySelectorAll(".naver");
-링크.forEach((e) => {
-  if (e instanceof HTMLAnchorElement) {
-    e.href = "https://kakao.com";
-  }
-});
+let 사람1 = new Person("kim");
+let 사람2 = new Person("park");
 
-let 버튼 = document.querySelector("#button");
-
-let 이미지 = document.querySelector("#image");
-버튼?.addEventListener("click", function () {
-  if (이미지 instanceof HTMLImageElement) {
-    이미지.src = "new.jpg";
-  }
-});
+console.log(사람1.함수("어쩌구"));

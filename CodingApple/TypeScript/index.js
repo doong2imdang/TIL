b0b7e5a6338660dcc7a38fd654a21c67850ad5c8
KIm Seo Young <jmh7367@naver.com>
2024-10-08 16:48:18 +0900

    TypeScript 1.14. Object에 타입지정하려면 interface 이것도 있음 숙제 3

--- CodingApple/TypeScript/index.js
+++ CodingApple/TypeScript/index.js
@@ -1,4 +1,4 @@
 var 장바구니 = [
-    { product: "청소기", price: 7000 },
-    { product: "삼다수", price: 800 },
+    { product: "청소기", price: 7000, card: false },
+    { product: "삼다수", price: 800, card: true },
 ];
--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -2,8 +2,15 @@ interface ShoppingCart {
   product: string;
   price: number;
 }
+// interface ShoppingCart {
+//   card: boolean;
+// }
 
-let 장바구니: ShoppingCart[] = [
-  { product: "청소기", price: 7000 },
-  { product: "삼다수", price: 800 },
+interface NewShoppingCart extends ShoppingCart {
+  card: boolean;
+}
+
+let 장바구니: NewShoppingCart[] = [
+  { product: "청소기", price: 7000, card: false },
+  { product: "삼다수", price: 800, card: true },
 ];

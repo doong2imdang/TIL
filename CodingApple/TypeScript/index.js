fd6d80eb733344aadb9ee68c12fddb7e48b2077c
KIm Seo Young <jmh7367@naver.com>
2024-10-08 16:20:03 +0900

    TypeScript 1.14. Object에 타입지정하려면 interface 이것도 있음

--- CodingApple/TypeScript/index.js
+++ CodingApple/TypeScript/index.js
@@ -1,23 +1,7 @@
-var Word = /** @class */ (function () {
-    function Word() {
-        var param = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            param[_i] = arguments[_i];
-        }
-        var _this = this;
-        this.num = [];
-        this.str = [];
-        param.forEach(function (i) {
-            if (typeof i === "string") {
-                _this.str.push(i);
-            }
-            else {
-                _this.num.push(i);
-            }
-        });
-    }
-    return Word;
-}());
-var obj = new Word("kim", 3, 5, "park");
-console.log(obj.num); //[3,5]
-console.log(obj.str); //['kim', 'park']
+var 학생 = {
+    name: "kim",
+};
+var 선생 = {
+    name: "kim",
+    age: 20,
+};
--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -1,17 +1,19 @@
-class Word {
-  num: number[] = [];
-  str: string[] = [];
-  constructor(...param: (number | string)[]) {
-    param.forEach((i) => {
-      if (typeof i === "string") {
-        this.str.push(i);
-      } else {
-        this.num.push(i);
-      }
-    });
-  }
+type Animal = { name: string };
+type Cat = { age: number } & Animal;
+
+interface Student {
+  name: string;
 }
 
-let obj = new Word("kim", 3, 5, "park");
-console.log(obj.num); //[3,5]
-console.log(obj.str); //['kim', 'park']
+interface Teacher extends Student {
+  age: number;
+}
+
+let 학생: Student = {
+  name: "kim",
+};
+
+let 선생: Teacher = {
+  name: "kim",
+  age: 20,
+};

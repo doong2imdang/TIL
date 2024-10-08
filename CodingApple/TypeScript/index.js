8c7b50e62179bc2b1efad2d50671f7dd32d0e0f5
KIm Seo Young <jmh7367@naver.com>
2024-10-08 15:55:31 +0900

    TypeScript 1.13. class 만들 때 타입지정 가능 숙제2

--- CodingApple/TypeScript/index.js
+++ CodingApple/TypeScript/index.js
@@ -1,13 +1,23 @@
-var Car = /** @class */ (function () {
-    function Car(a, b) {
-        this.model = a;
-        this.price = b;
+var Word = /** @class */ (function () {
+    function Word() {
+        var param = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            param[_i] = arguments[_i];
+        }
+        var _this = this;
+        this.num = [];
+        this.str = [];
+        param.forEach(function (i) {
+            if (typeof i === "string") {
+                _this.str.push(i);
+            }
+            else {
+                _this.num.push(i);
+            }
+        });
     }
-    Car.prototype.tax = function () {
-        return this.price * 0.1;
-    };
-    return Car;
+    return Word;
 }());
-var car1 = new Car("소나타", 3000);
-console.log(car1); //콘솔창 출력결과는 { model : '소나타', price : 3000 }
-console.log(car1.tax()); //콘솔창 출력결과는 300
+var obj = new Word("kim", 3, 5, "park");
+console.log(obj.num); //[3,5]
+console.log(obj.str); //['kim', 'park']
--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -1,16 +1,17 @@
-class Car {
-  model: string;
-  price: number;
-  constructor(a: string, b: number) {
-    this.model = a;
-    this.price = b;
-  }
-
-  tax(): number {
-    return this.price * 0.1;
+class Word {
+  num: number[] = [];
+  str: string[] = [];
+  constructor(...param: (number | string)[]) {
+    param.forEach((i) => {
+      if (typeof i === "string") {
+        this.str.push(i);
+      } else {
+        this.num.push(i);
+      }
+    });
   }
 }
 
-let car1 = new Car("소나타", 3000);
-console.log(car1); //콘솔창 출력결과는 { model : '소나타', price : 3000 }
-console.log(car1.tax()); //콘솔창 출력결과는 300
+let obj = new Word("kim", 3, 5, "park");
+console.log(obj.num); //[3,5]
+console.log(obj.str); //['kim', 'park']

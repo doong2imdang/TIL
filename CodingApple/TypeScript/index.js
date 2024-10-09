2973b1e8500cae34ea1baea74d5495b2111903b2
KIm Seo Young <jmh7367@naver.com>
2024-10-09 17:40:33 +0900

    TypeScript 2.1. 함수 rest 파라미터, destructuring 할 때 타입지정 숙제1

--- CodingApple/TypeScript/index.js
+++ CodingApple/TypeScript/index.js
@@ -1,6 +1,19 @@
-var person = { student: true, age: 20 };
-function 함수(_a) {
-    var student = _a.student, age = _a.age;
-    console.log(student, age);
+function 함수() {
+    var a = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        a[_i] = arguments[_i];
+    }
+    var 제일큰수 = 0;
+    a.forEach(function (e) {
+        if (제일큰수 < e) {
+            제일큰수 = e;
+        }
+    });
+    return 제일큰수;
 }
-함수({ student: true, age: 20 });
+console.log(함수(6, 8, 7, 2));
+// function 함수(...a: number[]): number {
+//   let 제일큰수 = Math.max(...a);
+//   return 제일큰수;
+// }
+// console.log(함수(6, 3, 7, 2));
--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -1,7 +1,20 @@
-let person = { student: true, age: 20 };
+function 함수(...a: number[]): number {
+  let 제일큰수 = 0;
 
-function 함수({ student, age }: { student: boolean; age: number }) {
-  console.log(student, age);
+  a.forEach((e) => {
+    if (제일큰수 < e) {
+      제일큰수 = e;
+    }
+  });
+
+  return 제일큰수;
 }
 
-함수({ student: true, age: 20 });
+console.log(함수(6, 8, 7, 2));
+
+// function 함수(...a: number[]): number {
+//   let 제일큰수 = Math.max(...a);
+//   return 제일큰수;
+// }
+
+// console.log(함수(6, 3, 7, 2));

8c2356ed91989f4304e98636b4a76233d8caedde
KIm Seo Young <jmh7367@naver.com>
2024-10-09 17:36:07 +0900

    TypeScript 2.1. 함수 rest 파라미터, destructuring 할 때 타입지정 과제

--- CodingApple/TypeScript/index.js
+++ CodingApple/TypeScript/index.js
@@ -1,25 +1,6 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-function 함수() {
-    var a = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        a[_i] = arguments[_i];
-    }
-    console.log(a);
+var person = { student: true, age: 20 };
+function 함수(_a) {
+    var student = _a.student, age = _a.age;
+    console.log(student, age);
 }
-함수(1, 5, 2, 5, 6, "6", true);
-var arr = [1, 2, 3];
-var arr2 = [4, 5];
-var arr3 = __spreadArray(__spreadArray([], arr, true), arr2, true);
-console.log(arr3);
-var _a = ["안녕", 100], 변수1 = _a[0], 변수2 = _a[1];
-console.log(변수1, 변수2);
-var _b = { student: true, age: 20 }, student = _b.student, age = _b.age;
-console.log(student, age);
+함수({ student: true, age: 20 });
--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -1,16 +1,7 @@
-function 함수(...a: (number | string | boolean)[]) {
-  console.log(a);
+let person = { student: true, age: 20 };
+
+function 함수({ student, age }: { student: boolean; age: number }) {
+  console.log(student, age);
 }
 
-함수(1, 5, 2, 5, 6, "6", true);
-
-let arr = [1, 2, 3];
-let arr2 = [4, 5];
-let arr3 = [...arr, ...arr2];
-console.log(arr3);
-
-let [변수1, 변수2] = ["안녕", 100];
-console.log(변수1, 변수2);
-
-let { student, age } = { student: true, age: 20 };
-console.log(student, age);
+함수({ student: true, age: 20 });

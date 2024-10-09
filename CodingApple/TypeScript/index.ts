117376be98340c770c6abac43b34b6f10babf225
KIm Seo Young <jmh7367@naver.com>
2024-10-09 17:43:25 +0900

    TypeScript 2.1. 함수 rest 파라미터, destructuring 할 때 타입지정 숙제 2

--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -1,20 +1,7 @@
-function 함수(...a: number[]): number {
-  let 제일큰수 = 0;
+type Info = { user: string; comment: number[]; admin: boolean };
 
-  a.forEach((e) => {
-    if (제일큰수 < e) {
-      제일큰수 = e;
-    }
-  });
-
-  return 제일큰수;
+function 함수({ user, comment, admin }: Info): void {
+  console.log(user, comment, admin);
 }
 
-console.log(함수(6, 8, 7, 2));
-
-// function 함수(...a: number[]): number {
-//   let 제일큰수 = Math.max(...a);
-//   return 제일큰수;
-// }
-
-// console.log(함수(6, 3, 7, 2));
+함수({ user: "kim", comment: [3, 5, 4], admin: false });

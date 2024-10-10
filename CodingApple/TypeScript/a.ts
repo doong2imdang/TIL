536966d16c3c714d22c366d27e3c119e9357f8b5
KIm Seo Young <jmh7367@naver.com>
2024-10-10 12:46:33 +0900

    TypeScript 2.7. 타입도 import export 해서 씁니다 그리고 namespace

---
+++ CodingApple/TypeScript/a.ts
@@ -0,0 +1,7 @@
+export var 이름 = "kim";
+export var 나이 = 20;
+export type 어쩌구 = string;
+export interface 인터페이스 {}
+
+namespace 네임스페이스 {}
+module 네임스페이스 {}
--- CodingApple/TypeScript/index.js
+++ CodingApple/TypeScript/index.js
@@ -1,40 +1,4 @@
-// class Square {
-//   width: number;
-//   height: number;
-//   color: string;
-//   constructor(a: number, b: number, c: string) {
-//     this.width = a;
-//     this.height = b;
-//     this.color = c;
-//   }
-//   draw() {
-//     const container = document.querySelector(".container");
-//     const newDiv = document.createElement("div");
-//     newDiv.style.width = `${this.width}px`;
-//     newDiv.style.height = `${this.height}px`;
-//     newDiv.style.backgroundColor = this.color;
-//     container?.appendChild(newDiv);
-//   }
-// }
-var Square = /** @class */ (function () {
-    function Square(width, height, color) {
-        this.width = width;
-        this.height = height;
-        this.color = color;
-    }
-    Square.prototype.draw = function () {
-        var a = Math.random();
-        var square = "<div style=\"position:relative; \n      top:".concat(a * 400, "px; \n      left:").concat(a * 400, "px; \n      width:").concat(this.width, "px; \n      height : ").concat(this.height, "px; \n      background:").concat(this.color, "\"></div>");
-        document.body.insertAdjacentHTML("beforeend", square);
-    };
-    return Square;
-}());
-var 네모 = new Square(30, 30, "red");
-네모.draw();
-네모.draw();
-네모.draw();
-네모.draw();
-네모.draw();
-네모.draw();
-네모.draw();
-네모.draw();
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a_1 = require("./a");
+console.log(a_1.이름, a_1.나이);

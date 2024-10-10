dde899b353c9c976a9073dbe3124756b528251a4
KIm Seo Young <jmh7367@naver.com>
2024-10-10 22:35:51 +0900

    TypeScript 2.11. array 자료에 붙일 수 있는 tuple type 숙제1

--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -1,14 +1,1 @@
-let 멍멍: [string, boolean?, number?] = ["dog"];
-
-// function 함수(...x: [number, string]) {
-//   console.log(x);
-// }
-
-function 함수(a: number, b: string) {
-  console.log([a, b]);
-}
-
-함수(111, "222");
-
-let arr = [1, 2, 3];
-let arr2: [number, number, ...number[]] = [4, 5, ...arr];
+let arr: [string, number, boolean] = ["꼬북칩", 1800, true];

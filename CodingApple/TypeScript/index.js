b239e68fccb763b2797906afbd4dad45af701f91
KIm Seo Young <jmh7367@naver.com>
2024-10-10 15:51:43 +0900

    TypeScript 2.8. 타입을 파라미터로 입력하는 Generic 숙제1

--- CodingApple/TypeScript/index.js
+++ CodingApple/TypeScript/index.js
@@ -1,5 +1,6 @@
 function 함수(x) {
     return x.length;
 }
-var a = 함수(["100"]);
-console.log(a);
+var a = 함수("hello");
+var b = 함수(["kim", "park"]);
+console.log(a, b);
--- CodingApple/TypeScript/index.ts
+++ CodingApple/TypeScript/index.ts
@@ -1,11 +1,8 @@
-interface LengthCheck {
-  length: number;
-}
-
-function 함수<MyType extends LengthCheck>(x: MyType) {
+function 함수<T extends string | string[]>(x: T) {
   return x.length;
 }
 
-let a = 함수<string[]>(["100"]);
+let a = 함수<string>("hello");
+let b = 함수<string[]>(["kim", "park"]);
 
-console.log(a);
+console.log(a, b);
